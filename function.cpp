4a83ca689f8d4e2f8acfde8b1cb70059899b5058
1702025 <msalman98@yahoo.com>
2018-09-22 23:30:08 +0500

    second time

--- function.cpp
+++ function.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
+#include "header.h"
 int reversing_arry(int size ,int arry[])
 {
   int var1,var2,temp;
-  for(var1=0,var1<size,var1++)
+  for(var1=0;var1<size;var1++)
   {
     for(var2=var1+1;var2<size;var2++)
     {
@@ -16,7 +18,7 @@ int reversing_arry(int size ,int arry[])
 int sort_arry(int size,int arry[])
 {
   int var1,var2,temp;
-  for(var1=0,var1<size,var1++)
+  for(var1=0;var1<size;var1++)
   {
     for(var2=var1+1;var2<size;var2++)
     {
@@ -31,11 +33,39 @@ int sort_arry(int size,int arry[])
 }
 
 
-int search_sorted_arry(int size;int arry[])
+int search_unsorted_arry(int size,int arry[])
 {
-  int num;
-  cout<<"Enter the search number = ";
-  cin>>num;
-  for()
+  int num,var;
+  std::cout<<"Enter the search number = ";
+  std::cin>>num;
+  for(var=0;var<size;var++)
+  {
+    if(arry[var]==num)
+    {
+      std::cout<<"searching number is = "<<num;
+    }
+  }
+  if(arry[var]!=0)
+  {
+    std::cout<<"not valid ";
+  }
+}
 
+
+int search_sorted_arry(int size,int arry[],int num,int var)
+{
+   if(arry[var]==num)
+   {
+     std::cout<<num;
+     return 0;
+   }
+   else if(var++==num)
+   {
+     search_sorted_arry(size,arry,num,var);
+   }
+   else if(var<size)
+   {
+     std::cout<<"not found";
+     return 0;
+   }
 }
--- main.cpp
+++ main.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
+#include "header.h"
 using namespace std;
 int main()
 {
   int size,var;
-  cout<<"Enter your array size = ";
-  cin>>size;
+  std::cout<<"Enter your array size = ";
+  std::cin>>size;
   int arry[size];
   for(var=0;var<size;var++)
   {
-    cout<<"Enter array number "<<var<<" = ";
-    cin>>arry[var];
+    std::cout<<"Enter array number "<<var<<" = ";
+    std::cin>>arry[var];
   }
+  std::cout<<"1 = int reversing_arry(int size,int arry[]) "<<endl;
+  std::cout<<"2 = int sort_arry(int size,int arry []) "<<endl;
+  std::cout<<"3 = int search_unsorted_arry(int size,int arry[])"<<endl;
+  std::cout<<"4 = int int search_sorted_arry(int size,int arry[])"<<endl;
+  std::cout<<"5 = int rotatae_arry(int size,int arry[])"<<endl;
+  int user;
+  std::cout<<"enter using opearation = ";
+  std::cin>>user;
+  if(user==1)
+  {
+    reversing_arry(size,arry);
+  }
+  if(user==2)
+  {
+    sort_arry(size,arry);
+  }
+  if(user==3)
+  {
+    search_unsorted_arry(size,arry);
+  }
+  if(user==4)
+  {
+    int var=0,num;
+    std::cout<<"enter your number = ";
+    std::cin>>num;
+    search_sorted_arry(size,arry,num,var);
+  }
+
 }
