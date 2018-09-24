d30059c05863028c6711402e6846bb469620ae0c
1702025 <msalman98@yahoo.com>
2018-09-24 19:22:19 +0500

    third time

--- header.h
+++ header.h
@@ -1,5 +1,5 @@
 int reversing_arry(int size,int arry[]);
 int rotatae_arry(int size,int arry[]);
 int sort_arry(int size,int arry []);
-int search_unsorted_arry(int size,int arry[]);
-int search_sorted_arry(int size,int arry[]);
+int search_unsorted_arry(int size,int arry[],int num);
+int search_sorted_arry(int size,int arry[],int num,int var);
